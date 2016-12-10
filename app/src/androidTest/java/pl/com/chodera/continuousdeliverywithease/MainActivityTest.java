f0509c4ab39c22b94a963b21de9d1e05d64f378b
Adam Chodera <adam@onegini.com>
2016-12-10 15:44:47 +0100

    Create resource string for welcome text and fix test

--- app/src/androidTest/java/pl/com/chodera/continuousdeliverywithease/MainActivityTest.java
+++ app/src/androidTest/java/pl/com/chodera/continuousdeliverywithease/MainActivityTest.java
@@ -1,11 +1,13 @@
 package pl.com.chodera.continuousdeliverywithease;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import org.junit.Rule;
 import org.junit.Test;
 
+import android.content.res.Resources;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
@@ -20,9 +22,11 @@ public class MainActivityTest {
   public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
 
   @Test
-  public void shouldDisplayHelloWorld() {
-    Espresso.onView(ViewMatchers.withId(R.id.id_text_view))
-        .check(matches(withText("")));
+  public void shouldDisplayWelcomeUserText() {
+    Resources res = getInstrumentation().getTargetContext().getResources();
+    String welcomeUserText = res.getString(R.string.activity_main_welcome_user_text); // get a string resource
 
+    Espresso.onView(ViewMatchers.withId(R.id.id_text_view))
+        .check(matches(withText(welcomeUserText)));
   }
 }
